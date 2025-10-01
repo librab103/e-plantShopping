cd21167d7e716cae56d2e1477c4575bfa8b34f86
Matt Battle <librab103@gmail.com>
2025-10-01 01:40:53 -0400

    Updates 10-1-25

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
-import {} from
+
 
 export const CartSlice = createSlice({
   name: 'cart',
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -215,6 +215,7 @@ function ProductList({ onHomeClick }) {
             ]
         }
     ];
+    
     const styleObj = {
         backgroundColor: '#4CAF50',
         color: '#fff!important',
@@ -224,12 +225,14 @@ function ProductList({ onHomeClick }) {
         alignIems: 'center',
         fontSize: '20px',
     }
+
     const styleObjUl = {
         display: 'flex',
         justifyContent: 'space-between',
         alignItems: 'center',
         width: '1100px',
     }
+
     const styleA = {
         color: 'white',
         fontSize: '30px',
@@ -245,6 +248,7 @@ function ProductList({ onHomeClick }) {
         e.preventDefault();
         setShowCart(true); // Set showCart to true when cart icon is clicked
     };
+
     const handlePlantsClick = (e) => {
         e.preventDefault();
         setShowPlants(true); // Set showAboutUs to true when "About Us" link is clicked
