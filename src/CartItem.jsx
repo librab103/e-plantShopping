cc183c1baafad09c47842c1f006ef9f921ed60cf
Matt Battle <librab103@gmail.com>
2025-10-01 04:35:25 -0400

    Shopping cart update v2.0

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -24,20 +24,24 @@ const CartItem = ({ onContinueShopping }) => {
     };
 
     const handleIncrement = (item) => {
-
+        dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
     };
 
     const handleDecrement = (item) => {
-   
+        if (item.quantity > 1) {
+            dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+        } else {
+            dispatch(removeItem(item.name))
+        }
     };
 
     const handleRemove = (item) => {
-
+        dispatch(removeItem(item.name))
     };
 
   // Calculate total cost based on quantity for an item
     const calculateTotalCost = (item) => {
-
+        return parseFloat(item.cost.substring(1)) * item.quantity;
     };
 
     const handleCheckoutShopping = (e) => {
